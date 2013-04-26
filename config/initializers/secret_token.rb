ee3563890457d75f922ee6a5b3b72e9229fd9f54
Andre Salmon <Salmon.andre.c@gmail.com>
2013-04-26 11:49:10 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jumpline2::Application.config.secret_token = 'b87d5272daa4dc1f1ff96d283225561290f0fd568d2dabd20d5abcddc63dbb011adb1643cc4eed541b9cf896dd9fb3bb214a5d5c031a2d7e2ee807c244c1c8bb'
